5343fc23b3fdf8e00d9c5af67645c85ca2509aac
TMToast <tristanlmartin95@gmail.com>
2017-12-16 23:56:18 -0800

    Initial commit

---
+++ app/src/test/java/com/example/trist/code_name_feast/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.trist.code_name_feast;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
